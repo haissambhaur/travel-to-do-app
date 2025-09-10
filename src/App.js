0331a4c7b665adff5a5d443ca1e5833ffd276a4b
haissambhaur <sambhaur@example.com>
2025-09-10 06:03:03 -0700

    Feat(Form): Form for taking items as an input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 
 export default function App(){
   return <div className="app">
     <Logo/>
     <Form/>
-    <PakcingList/>
+    <PackingList data={initialItems}/>
     <Stats/>
   </div>
 }
@@ -17,14 +17,41 @@ function Logo(){
 }
 
 function Form(){
-  return <div className="add-form">
+  return <form className="add-form">
     <h3>what do you need for your trip?</h3>
-  </div>
+    <select>
+      {Array.from({length:20}, (_,i)=>(
+        <option key={i+1}>
+          {i+1}
+        </option>
+      ))}
+    </select>
+    <input type="text" placeholder="item..."/>
+    <button>Add</button>
+  </form>
   
 }
 
-function PakcingList(){
-  return <div className="list">LIST</div>
+function PackingList({data}){
+  //console.log(data)
+  return <div  className="list">
+    <ul>
+    {data.map((item)=>(
+      //console.log(item)
+      <Item key= {item.id} item= {item}/>
+      
+    ))}
+  </ul>
+  </div>
+}
+
+function Item({item}){
+  return(
+    <li >
+      <span style={item.packed? {textDecoration: "line-through"} : {}}>{item.description} {item.quantity}</span>
+      <button>❌</button>
+      </li>
+  )
 }
 
 function Stats(){

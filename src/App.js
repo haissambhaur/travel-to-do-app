acb7d45f71010621f7c04d4856136eeef8cf9403
haissambhaur <sambhaur@example.com>
2025-09-16 15:35:34 +0500

    Feat(State controlled elements): binding ui elements with states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
+import { useState } from "react";
+
+// let initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+// ];
 
 export default function App(){
+  const [item, setItem] = useState([])
   return <div className="app">
     <Logo/>
-    <Form/>
-    <PackingList data={initialItems}/>
+    <Form items = {item} setItem = {setItem}/>
+    <PackingList data={item}/>
     <Stats/>
   </div>
 }
@@ -16,17 +19,54 @@ function Logo(){
   return <h1>🌴Far Away</h1>
 }
 
-function Form(){
-  return <form className="add-form">
+function Form({items, setItem}){
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+  
+  function handlingSubmmit (e){
+      e.preventDefault();
+      
+      if(!description) return;
+
+      const newItem = {
+        id: Date.now(),
+        description,
+        quantity,
+        packed: false
+      };
+      // console.log(newItem)
+      setItem([...items, newItem])
+
+      setDescription("")
+      setQuantity(1)
+  }
+  return <form className="add-form" onSubmit={handlingSubmmit}>
     <h3>what do you need for your trip?</h3>
-    <select>
+    {/* <select>
       {Array.from({length:20}, (_,i)=>(
         <option key={i+1}>
           {i+1}
         </option>
       ))}
+    </select> */}
+    <select value={quantity} onChange={(e)=> {
+      console.log(Number(e.target.value))
+      setQuantity(Number(e.target.value));
+    }}>
+      {Array.from({length:20}, (_, i)=>i+1).map
+      ((num) => (
+        <option key={num} value={num}>
+          {num}
+          </option>
+      ))}
     </select>
-    <input type="text" placeholder="item..."/>
+    <input 
+      type="text" 
+      placeholder="item..." 
+      value={description} 
+      onChange={(e)=>{
+      // console.log(e.target.value);
+      setDescription(e.target.value);}}/>
     <button>Add</button>
   </form>
   
